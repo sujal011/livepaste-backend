64859755f7aff62676a087d114b2bca5b555295d
Sujal Bhavsar <sujalofficial011@gmail.com>
2024-10-30 12:39:22 +0530

    Update WebConfig.java
    added domains for rest apis

--- src/main/java/com/sujal/livepaste/configs/WebConfig.java
+++ src/main/java/com/sujal/livepaste/configs/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173","https://livepaste.sujalbhavsar.in","https://live-paste-frontend.vercel.app")
+                .allowedOrigins("http://localhost:5173","https://livepaste.sujalbhavsar.in","https://live-paste-frontend.vercel.app","https://livepaste.netlify.app","https://livepaste.vercel.app")
                 .allowedMethods("GET", "POST", "PUT")
                 .allowCredentials(true);
     }
